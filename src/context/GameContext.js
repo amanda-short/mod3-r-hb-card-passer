d3d2a158382b49a77a4529b1a3b7298fe763545d
amanda-short <AmandaShort@Kamies-MacBook-Pro.local>
2023-01-25 14:18:21 -0800

    GameProvider and gameObject built

--- src/context/GameContext.js
+++ src/context/GameContext.js
@@ -1,11 +1,35 @@
-import { useState } from 'react';
-import { createContext } from 'react';
+import { useState, createContext, useGameContext } from 'react';
+import initialCards from '../cards-data.js';
 
 const GameContext = createContext();
 
 const GameProvider = ({ children }) => {
-  const [deck, setDeck] = useState('');
-  return <GameContext.Provider value={{ deck, setDeck }}>{children}</GameContext.Provider>;
+  const [deck, setDeck] = useState(initialCards);
+  const [playerOneHand, setPlayerOneHand] = useState([]);
+  const [selectedCard, setSelectedCard] = useState();
+  const [playerTwoHand, setPlayerTwoHand] = useState([]);
+  const [playerThreeHand, setPlayerThreeHand] = useState([]);
+  const [from, setFrom] = useState('deck');
+  const [to, setTo] = useState(1);
+
+  const gameObject = {
+    deck,
+    setDeck,
+    playerOneHand,
+    setPlayerOneHand,
+    selectedCard,
+    setSelectedCard,
+    playerTwoHand,
+    setPlayerTwoHand,
+    playerThreeHand,
+    setPlayerThreeHand,
+    from,
+    setFrom,
+    to,
+    setTo,
+  };
+
+  return <GameContext.Provider value={{ gameObject }}>{children}</GameContext.Provider>;
 };
 
-export { GameProvider, GameContext };
+export { GameProvider, useGameContext };
